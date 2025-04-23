a1dd6314cd03a3765030a5375bb20b5593feb105
Abu Bakkar Siddique <46748540+codesdeal@users.noreply.github.com>
2025-04-23 21:24:38 +0600

    Update enqueue-asstes.php

--- inc/enqueue-asstes.php
+++ inc/enqueue-asstes.php
@@ -1,25 +1,13 @@
 <?php
 function _themename_assets() {
-    wp_enqueue_style('devabu-style', get_stylesheet_uri(), array(), '1.0.0', 'all');
-    wp_enqueue_style('devabu-stylesheet', get_template_directory_uri() . '/dist/assets/css/bundle.css', array(), '1.0.0', 'all');
-    
-    // Load FontAwesome from CDN
-    wp_enqueue_style('fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css', array(), '6.7.2', 'all');
-    
-    // Add Swiper CSS
-    wp_enqueue_style('swiper-css', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css', array(), '11.0.0', 'all');
+    wp_enqueue_style('_themename-style', get_stylesheet_uri(), array(), '1.0.0', 'all');
+    wp_enqueue_style('_themename-stylesheet', get_template_directory_uri() . '/dist/assets/css/bundle.css', array(), '1.0.0', 'all');
     
     wp_enqueue_script('jquery');
-
-    // Add Isotope JS before our bundle with jQuery as dependency
-    wp_enqueue_script('isotope-js', 'https://unpkg.com/isotope-layout@3.0.6/dist/isotope.pkgd.min.js', array('jquery'), '3.0.6', false);
     
-    // Add Swiper JS before our bundle
-    wp_enqueue_script('swiper-js', 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', array(), '11.0.0', true);
-    
-    wp_enqueue_script('devabu-scripts', get_template_directory_uri() . '/dist/assets/js/bundle.js', array('jquery', 'swiper-js', 'isotope-js'), '1.0.0', true);
+    wp_enqueue_script('_themename-scripts', get_template_directory_uri() . '/dist/assets/js/bundle.js', array('jquery'), '1.0.0', true);
 
-    wp_localize_script('devabu-scripts', '_themename', array(
+    wp_localize_script('_themename-scripts', '_themename', array(
         'nonce' => wp_create_nonce('theme_error_logging'),
         'ajaxurl' => admin_url('admin-ajax.php')
     ));
@@ -27,21 +15,14 @@ function _themename_assets() {
 add_action('wp_enqueue_scripts', '_themename_assets');
 
 function _themename_admin_assets() {
-    wp_enqueue_style('devabu-admin-stylesheet', get_template_directory_uri() . '/dist/assets/css/admin.css', array(), '1.0.0', 'all');
+    wp_enqueue_style('_themename-admin-stylesheet', get_template_directory_uri() . '/dist/assets/css/admin.css', array(), '1.0.0', 'all');
 
-    wp_enqueue_script('devabu-admin-scripts', get_template_directory_uri() . '/dist/assets/js/admin.js', array('jquery'), '1.0.0', true);
+    wp_enqueue_script('_themename-admin-scripts', get_template_directory_uri() . '/dist/assets/js/admin.js', array('jquery'), '1.0.0', true);
 }
 add_action('admin_enqueue_scripts', '_themename_admin_assets');
 
 
 function _themename_customize_preview_js() {
-    wp_enqueue_script( 'devabu-customize-preview', get_template_directory_uri() . '/dist/assets/js/customize-preview.js', array( 'customize-preview', 'jquery' ), '1.0.0', true );
+    wp_enqueue_script( '_themename-customize-preview', get_template_directory_uri() . '/dist/assets/js/customize-preview.js', array( 'customize-preview', 'jquery' ), '1.0.0', true );
 }
 add_action( 'customize_preview_init', '_themename_customize_preview_js' );
-
-// Add Google Font
-function _themename_add_google_font_fontawesome() {
-    // wp_enqueue_style('_themename_google_font', 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap', false);
-    // wp_enqueue_style('_themename_fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css', false);
-}
-add_action('wp_enqueue_scripts', '_themename_add_google_font_fontawesome');
