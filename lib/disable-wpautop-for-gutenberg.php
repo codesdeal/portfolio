dd96c7c661ce62f5cb9a0b49049138f72eaf2ef7
Abu Bakkar Siddique <46748540+codesdeal@users.noreply.github.com>
2025-05-08 07:01:17 +0600

    Update disable-wpautop-for-gutenberg.php

--- lib/disable-wpautop-for-gutenberg.php
+++ lib/disable-wpautop-for-gutenberg.php
@@ -1,28 +1,14 @@
 <?php
 /**
- * Disable wpautop for Gutenberg blocks.
+ * Disables wpautop to remove empty p tags in rendered Gutenberg blocks.
  *
  * @package _themename
  */
-/**
- * Disables wpautop to remove empty p tags in rendered Gutenberg blocks.
- *
- * @author Corey Collins
- */
-function disable_wpautop_for_gutenberg() {
+function _themename_disable_wpautop_for_gutenberg() {
 	// If we have blocks in place, don't add wpautop.
 	if ( has_filter( 'the_content', 'wpautop' ) && has_blocks() ) {
 		remove_filter( 'the_content', 'wpautop' );
 	}
 }
 
-add_filter( 'init', 'disable_wpautop_for_gutenberg', 9 );
-
-
-// function _themename_remove_wpautop_on_front_page() {
-// 	if ( is_front_page() ) {
-// 		remove_filter( 'the_content', 'wpautop' );
-// 		remove_filter( 'the_excerpt', 'wpautop' );
-// 	}
-// }
-// add_action( 'template_redirect', '_themename_remove_wpautop_on_front_page' );
+add_filter( 'init', '_themename_disable_wpautop_for_gutenberg', 9 );
